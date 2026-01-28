20b36a30378b37aa5f0fe151bd479b5a267882e9
Victor Henrique de Oliveira <victorhenrique121413@gmail.com>
2026-01-27 21:32:39 -0300

    Remove imports não utilizados

--- lib/app/app_widget.dart
+++ lib/app/app_widget.dart
@@ -1,9 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get_it/get_it.dart';
 import 'package:url_shortener_app/app/home/controllers/home_controller.dart';
-import 'package:url_shortener_app/app/home/data/repositories/home_repository.dart';
-import 'package:url_shortener_app/app/home/data/services/api/api_client.dart';
-import 'package:url_shortener_app/app/home/data/services/local/local_data_service.dart';
 
 import 'home/views/home_page.dart';
 
